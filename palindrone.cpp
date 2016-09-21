c4df88a5045ceed1965f4eff5c026ca069fa6bde
U-BSD\456570 <456570@SHS-10D8A6AA9.beaverton.k12.or.us>
2016-09-21 14:25:40 -0700

    changes to uppercase and removes spaces

--- palindrone.cpp
+++ palindrone.cpp
@@ -7,13 +7,27 @@ using namespace std;
 int main() {
   char input[81];
   char output [81];
+  char reverse [81];
+  int x = 0;
   cout << "Welcome to Palindrone. Please enter a sentence or a word." << endl;
   cout << "I will tell you if it's a palindrone." << endl;
   cin.get(input,81);
-  
+
   for (int i = 0; i<81;i++) {
-	input [i] = tolower(input[i]);
+    if (input[i] == '\0'){
+      i=82;
+      output[x] = '\0';
+    }
+    if (isalpha(input[i])){
+      output[x] = tolower(input[i]);
+      x++;
+      }
   }
-  cout << input;
+  /*for (int i = 0; i < 81; i++){ //or is it i<x
+    output [i] = reverse[x-i];
+  } doesnt work
+  */
+  
+  cout << output << endl;
   return 0;
 }
